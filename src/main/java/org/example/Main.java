42871cb8d88c145612182320cd9b742cf8eada34
MariaKolyda <iis20004@uom.edu.gr>
2023-04-30 16:50:27 +0300

    commit module

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,7 @@
 package org.example;
 
-import java.io.File;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.TransportException;
 public class Main {
     public static void main(String[] args) {
 
-            // TODO Auto-generated method stub
-            System.out.print("maria!!!!");
-            try {
-                Git.cloneRepository()
-                        .setURI("https://github.com/eclipse/jgit.git")
-                        .setDirectory(new File("/Desktop"))
-                        .call();
-            } catch (InvalidRemoteException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (TransportException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (GitAPIException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
         }
 }
